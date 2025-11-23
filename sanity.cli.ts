0aa1de374965a0d8b2d8a89d7f734f0fa797e7db
thesmokecurtainguy <thesmokecurtainguy@gmail.com>
2025-11-23 16:15:28 -0500

    fix: update studio hostname to sophron-studies

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,7 +5,7 @@ export default defineCliConfig({
     projectId: '7a9l1mtl',
     dataset: 'production'
   },
-  studioHost: 'sophronstudies',
+  studioHost: 'sophron-studies',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
